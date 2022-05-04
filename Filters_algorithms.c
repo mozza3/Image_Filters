dd66a2f647b6a2cc744e2ddbcad4790548a28412
Moaz Wagdy <moazwagdy159@gmail.com>
2022-05-04 11:34:32 +0200

    Create Filters_algorithms.c

---
+++ Filters_algorithms.c
@@ -0,0 +1,174 @@
+#include "helpers.h"
+#include <math.h>
+#include <stdlib.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int new_gvalue = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / (float) 3);
+            image[i][j].rgbtBlue = new_gvalue;
+            image[i][j].rgbtGreen = new_gvalue;
+            image[i][j].rgbtRed = new_gvalue;
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+
+            int sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+
+            int sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+
+
+            if (sepiaRed > 0xff)
+            {
+                sepiaRed = 0xff;
+            }
+            if (sepiaGreen > 0xff)
+            {
+                sepiaGreen = 0xff;
+            }
+            if (sepiaBlue > 0xff)
+            {
+                sepiaBlue = 0xff;
+            }
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE new_image[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < round(width / (float) 2); j++)
+        {
+            new_image[i][j] = image[i][j];
+            image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = new_image[i][j];
+        }
+    }
+
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE ogImage[height][width];
+    //
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            ogImage[i][j] = image[i][j];
+        }
+    }
+    //
+    for (int i = 0; i < height; i++)
+    {
+        //
+        for (int j = 0; j < width; j++)
+        {
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            int counter = 0;
+
+            if (i >= 0 && j >= 0)
+            {
+                red += ogImage[i][j].rgbtRed;//
+                green += ogImage[i][j].rgbtGreen;
+                blue += ogImage[i][j].rgbtBlue;
+                counter++;//
+            }
+            //
+            if (i >= 0 && j - 1 >= 0)
+            {
+                red += ogImage[i][j - 1].rgbtRed;
+                green += ogImage[i][j - 1].rgbtGreen;//
+                blue += ogImage[i][j - 1].rgbtBlue;
+                counter++; //
+            }
+            //
+            if ((i >= 0 && j + 1 >= 0) && (i >= 0 && j + 1 < width))
+            {
+                red += ogImage[i][j + 1].rgbtRed;
+                green += ogImage[i][j + 1].rgbtGreen;
+                blue += ogImage[i][j + 1].rgbtBlue;
+                counter++;
+            }
+            //
+            if (i - 1 >= 0 && j >= 0)
+            {
+                red += ogImage[i - 1][j].rgbtRed;
+                green += ogImage[i - 1][j].rgbtGreen;
+                blue += ogImage[i - 1][j].rgbtBlue;
+                counter++;
+            }
+            //
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                red += ogImage[i - 1][j - 1].rgbtRed;
+                green += ogImage[i - 1][j - 1].rgbtGreen;
+                blue += ogImage[i - 1][j - 1].rgbtBlue;
+                counter++;
+            }
+            //
+            if ((i - 1 >= 0 && j + 1 >= 0) && (i - 1 >= 0 && j + 1 < width))
+            {
+                red += ogImage[i - 1][j + 1].rgbtRed;
+                green += ogImage[i - 1][j + 1].rgbtGreen;
+                blue += ogImage[i - 1][j + 1].rgbtBlue;
+                counter++;
+            }
+            //
+            if ((i + 1 >= 0 && j >= 0) && (i + 1 < height && j >= 0))
+            {
+                red += ogImage[i + 1][j].rgbtRed;
+                green += ogImage[i + 1][j].rgbtGreen;
+                blue += ogImage[i + 1][j].rgbtBlue;
+                counter++;
+            }
+            //
+            if ((i + 1 >= 0 && j - 1 >= 0) && (i + 1 < height && j - 1 >= 0))
+            {
+                red += ogImage[i + 1][j - 1].rgbtRed;
+                green += ogImage[i + 1][j - 1].rgbtGreen;
+                blue += ogImage[i + 1][j - 1].rgbtBlue;
+                counter++;
+            }
+            if ((i + 1 >= 0 && j + 1 >= 0) && (i + 1 < height && j + 1 < width))
+            {
+                red += ogImage[i + 1][j + 1].rgbtRed;
+                green += ogImage[i + 1][j + 1].rgbtGreen;
+                blue += ogImage[i + 1][j + 1].rgbtBlue;
+                counter++;
+            }
+
+            image[i][j].rgbtRed = round(red / (counter * 1.0));
+            image[i][j].rgbtGreen = round(green / (counter * 1.0));
+            image[i][j].rgbtBlue = round(blue / (counter * 1.0));
+        }
+        //
+    }
+    return;
+}
